72ac549262ddb0534717e2ce61f980292483d556
Vinayashree-sn <vinayashreesn2@gmail.com>
2025-10-05 13:13:29 +0530

    test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,13 +1,5 @@
+def test_addition():
+    assert 2 + 2 == 4
 
-def test_add():
-    assert add(2, 3) == 5
-
-def test_subtract():
-    assert subtract(5, 3) == 2
-
-def test_multiply():
-    assert multiply(4, 3) == 12
-
-def test_divide():
-    assert divide(10, 2) == 5
-
+def test_subtraction():
+    assert 6 - 4 == 2
